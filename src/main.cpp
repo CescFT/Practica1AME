74a139ad92715b605c83bc4d4cc908b926efb6ad
Francesc Ferre <francesc.ferre@estudiants.urv.cat>
2019-11-19 18:31:18 +0100

    master: added source code.

--- src/main.cpp
+++ src/main.cpp
@@ -103,6 +103,44 @@ void punt_cardinal_led_on(double x, double y){
 }
 
 
+float position(float pos){
+    //Being:
+    //MAX_Y and MIN_Y, S and N
+    //MAX_X and MIN_Y, E and W
+    //CENTER_X and CENTER_Y, point zero
+    //defined as global
+    if(between(pos,NORTHY,CENTERY)) return equivalentScale(pos,NORTHY,CENTERY,'N');
+    if(between(pos,EASTX,CENTERX)) return equivalentScale(pos,EASTX,CENTERX,'E');
+    if(between(pos,SOUTHY,CENTERY)) return equivalentScale(pos,SOUTHY,CENTERY,'S');
+    if(between(pos,WESTX,CENTERX)) return equivalentScale(pos,WESTX,CENTERX,'W');
+    else return 0;
+}
+
+bool between(float val, float val_1, float val_2){
+    if(val_1 > val_2) return (val<val_1 && val>val_2);
+    if(val_2 > val_1) return (val>val_1 && val<val_2);
+    else return false;
+}
+
+float equivalentScale(float val, float val_1, float val_2, char orientation){
+    float proportion;
+    float final_pos;
+    proportion = abs(val_2)/abs(val_1);
+    final_pos = final_pos + 127 * proportion;
+    switch(orientation){
+        case 'N':
+            return final_pos;
+        case 'E':
+            return final_pos;
+        case 'S':
+            return -final_pos;
+        case 'W':
+            return -final_pos;
+    }
+}
+
+
+
 int main() {
 
   // put your setup code here, to run once:
