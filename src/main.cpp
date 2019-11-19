5ce7eec010353abc168d0d04d94dc3465a17212c
Francesc Ferre <francesc.ferre@estudiants.urv.cat>
2019-11-20 00:15:49 +0100

    master: modifications.

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
 #include <mbed.h>
 
 //Centre
-#define CENTERX 0.80
-#define CENTERY 0.818559 
-
+#define CENTERMIN 0.70
+#define CENTERMAX 0.80
+/*
 //NORD
 #define NORTHX 0.731624
 #define NORTHY 0.863492
@@ -20,6 +20,7 @@
 //WEST
 #define WESTX 0.81319
 #define WESTY 0.01782
+*/
 
 AnalogIn posX_joystick (A1); //IO
 AnalogIn posY_joystick (A0); //I1
@@ -40,105 +41,26 @@ void ini_leds(){
     //wait_ms(1500);
 }
 
-void punt_cardinal_led_on(double x, double y){
-  if(y>CENTERY)
-    {
-        //nord
-        led_nord.write(1);
-        led_sud.write(0);
-        led_est.write(0);
-        led_oest.write(0);
-    }else {
-        //sud
-        led_nord.write(0);
-        led_sud.write(1);
-        led_est.write(0);
-        led_oest.write(0);
-    }
-    if(x>CENTERX){
-          //Est
-        led_nord.write(0);
-        led_sud.write(0);
-        led_est.write(1);
-        led_oest.write(0);
-    }else{
-          //Oest
-        led_nord.write(0);
-        led_sud.write(0);
-        led_est.write(0);
-        led_oest.write(1);
-    }
-    
-    if(y>CENTERY)
-    {
-      if(x>CENTERX){
-        //nord est
-        led_nord.write(1);
-        led_sud.write(0);
-        led_est.write(1);
-        led_oest.write(0);
-      }else{
-        //nord oest
-        led_nord.write(1);
-        led_sud.write(0);
-        led_est.write(0);
-        led_oest.write(1);
-      }
-    }else{
-      if(x>CENTERX){
-        //sud est
-        led_nord.write(0);
-        led_sud.write(1);
-        led_est.write(1);
-        led_oest.write(0);
-      }else{
-        //sud oest
-        led_nord.write(0);
-        led_sud.write(1);
-        led_est.write(0);
-        led_oest.write(1);
-      }
-    }
+char calibration(double x, double y){
+  char value = 0;
+  if(x>CENTERMAX) value = value | (1 << 2); //Max x
+  else value = value | (1 << 0); //Min x
+  if(y>CENTERMAX) value = value | (1 << 3); //Max y
+  else value = value | (1 << 1); //Min y
+  
+  return value;
+}
+
+void punt_cardinal_led_on(char value){
+        
+        
+    led_nord.write((value & (1 << 3)));
+    led_sud.write((value & (1 << 1)));
+    led_est.write((value & (1 << 2)));
+    led_oest.write((value & (1 << 0)));
+  
     wait_ms(1500);
 }
-
-
-float position(float pos){
-    //Being:
-    //MAX_Y and MIN_Y, S and N
-    //MAX_X and MIN_Y, E and W
-    //CENTER_X and CENTER_Y, point zero
-    //defined as global
-    if(between(pos,NORTHY,CENTERY)) return equivalentScale(pos,NORTHY,CENTERY,'N');
-    if(between(pos,EASTX,CENTERX)) return equivalentScale(pos,EASTX,CENTERX,'E');
-    if(between(pos,SOUTHY,CENTERY)) return equivalentScale(pos,SOUTHY,CENTERY,'S');
-    if(between(pos,WESTX,CENTERX)) return equivalentScale(pos,WESTX,CENTERX,'W');
-    else return 0;
-}
-
-bool between(float val, float val_1, float val_2){
-    if(val_1 > val_2) return (val<val_1 && val>val_2);
-    if(val_2 > val_1) return (val>val_1 && val<val_2);
-    else return false;
-}
-
-float equivalentScale(float val, float val_1, float val_2, char orientation){
-    float proportion;
-    float final_pos;
-    proportion = abs(val_2)/abs(val_1);
-    final_pos = final_pos + 127 * proportion;
-    switch(orientation){
-        case 'N':
-            return final_pos;
-        case 'E':
-            return final_pos;
-        case 'S':
-            return -final_pos;
-        case 'W':
-            return -final_pos;
-    }
-}
-
 
 
 int main() {
@@ -146,6 +68,7 @@ int main() {
   // put your setup code here, to run once:
   char posX[10];
   char posY[10];
+  char sortida[10];
   
 
   while(1) {
@@ -159,8 +82,17 @@ int main() {
     serial1.printf(posY);
 
     ini_leds();
-        
-    punt_cardinal_led_on(x, y);
+    char val =calibration(x, y);
+    sprintf(sortida, "res: %c\n", val);
+    serial1.printf(sortida);
+      
+      punt_cardinal_led_on(val);
+    //punt_cardinal_led_on(calibration(x, y));
+    /*
+    double num = position(x);
+    sprintf(sortida, "res: %lf\n", num);
+    serial1.printf(sortida);
+    */
 
   }
 }
