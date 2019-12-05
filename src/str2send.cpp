2a41cf3cbbdb6316a86a6d1490a5e6622fee186b
Guillem FP <guillemfriweb@gmail.com>
2019-12-05 12:07:30 +0100

    Update str2send.cpp

--- src/str2send.cpp
+++ src/str2send.cpp
@@ -6,32 +6,42 @@ Serial s(USBTX, USBRX);
 
 StringToSend::StringToSend(int _xI, int _yI,char _x[], char _y[], bool _buttonPressed){
     
+    int aux;
     char signe='a';
     
-    if(!determinarSigneNombre(_xI)) 
-        signe='+';
-    
-    if(signe=='+')
-        sprintf(x, "%c%d\0",signe, _x);
-    else
-        sprintf(x, "%d\0", _x);
-    
-    signe='a';
+    strcpy(resultat, "DATA,");
+    if (!determinarSigneNombre(_xI)){
+        strcat(resultat, "+");
+    }else{
+        strcat(resultat, "-");
+        _xI = _xI * (-1);
+    }
+    aux = fillZerosCounter(_xI);
+    for (int i = 0; i < aux; i = i + 1){
+        strcat(resultat, "0");
+    }
+    sprintf(x, "%d,", _xI);
+    strcat(resultat, x);
 
-    if(!determinarSigneNombre(_yI))   
-        signe='+';
-    
-    if(signe=='+')
-        sprintf(y, "%c%d\0",signe, _y);
-    else 
-        sprintf(y, "%d\0", _y);
-
+    if (!determinarSigneNombre(_yI)){
+        strcat(resultat, "+");
+    }else{
+        strcat(resultat, "-");
+        _yI = _yI * (-1);
+    }
+    aux = fillZerosCounter(_yI);
+    for (int i = 0; i < aux; i = i + 1){
+        strcat(resultat, "0");
+    }
+    sprintf(y, "%d,%d", _yI, _buttonPressed);
+    strcat(resultat, y);
     
     xI=_xI;
     yI=_yI;
-    processarReal(x, true);
+    
+    /*processarReal(x, true);
     processarReal(y, false);
-    bButtonPressed = _buttonPressed;
+    bButtonPressed = _buttonPressed;*/
     
 }
 
@@ -44,7 +54,7 @@ bool StringToSend::determinarSigneNombre(int n){//esNegatiu
         return n<0;
 }
 
-
+/*
 void StringToSend::processarReal(char val[], bool esX){
     int i= 0;
     int numReal=0;
@@ -88,8 +98,17 @@ void StringToSend::processarReal(char val[], bool esX){
         yReal[5]='\0';
     }
     
-}
+}*/
 
+int StringToSend::fillZerosCounter(int val){
+    int cont = 2;
+    while (val > 10)
+    {
+        cont = cont - 1;
+        val = val / 10;
+    }
+    return cont;
+}
 
 void StringToSend::generateString(){
     strcat(resultat, xReal);
